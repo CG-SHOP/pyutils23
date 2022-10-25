6ff52c9b0685f2dcd822402671d4572383502285
Dominik Krupke <d-krupke@users.noreply.github.com>
2022-10-25 12:05:40 +0200

    Update install.sh
    Fallback --user, fixing issue #4

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@
 # If all dependencies are already installed, you can also just run `python3 setup.py install`.
 pip install -r requirements.txt || exit  # python dependencies
 conan install . --build=missing -if cmake || exit  # c++ dependencies
-python3 setup.py install  # triggers building and installing the python module
+python3 setup.py install || python3 setup.py install --user # triggers building and installing the python module. Fallback to user mode.
